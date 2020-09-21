5c20874a57d5e9e30ec31504a1e9a0024ed0f64a
danny <540021730@qq.com>
2020-09-21 22:34:13 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './styles/index.less'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+import 'amfe-flexible'
 
 Vue.config.productionTip = false
 
@@ -11,3 +14,5 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
+
+Vue.use(Vant)
