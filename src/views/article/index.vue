33d81d2f6521e4069c6b26870d427301e115bca3
danny <540021730@qq.com>
2020-11-09 13:28:36 +0800

    文章详情页路由设置并传动态参数

---
+++ src/views/article/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="article-container">
+    文章详情
+  </div>
+</template>
+
+<script>
+// 在组件中获取动态路由参数：
+//    方式一：this.$route.params.articleId
+//    方式二：props 传参，推荐
+//      this.articleId
+
+export default {
+  name: 'ArticleIndex',
+  components: {},
+  props: {
+    articleId: {
+      type: String,
+      required: true
+    }
+  },
+  data () {
+    return {
+    }
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  methods: {
+  }
+}
+</script>
+
+<style scoped lang="less">
+.Search-result{
+  position: fixed;
+  top: 54px;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  overflow-y: auto;
+}
+</style>
