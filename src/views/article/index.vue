141ffbf059d5e7f5cc70bd7cb5cea0d59389b6e9
danny <540021730@qq.com>
2020-11-15 20:50:32 +0800

    慢速网络防止多次点击

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -25,15 +25,22 @@
       size="small"
       class="follow-btn"
       :icon="article.is_followed ? '' :'plus'"
+      @click="onFollow"
+      :loading="isFollowLoading"
       >
        {{article.is_followed ? '已关注' : '关注'}}</van-button>
     </van-cell>
-    <div class="markdown-body" v-html="article.content">
+    <div class="markdown-body"
+    v-html="article.content"
+    ref="article-content"
+    >
     </div>
   </div>
 </template>
 
 <script>
+import { ImagePreview } from 'vant'
+import { addFollow, deleteFollow } from '@/api/user'
 import {
   getArticleById
 } from '@/api/article'
@@ -54,7 +61,8 @@ export default {
   },
   data () {
     return {
-      article: {}
+      article: {},
+      isFollowLoading: false // 关注用户按钮的 loading 状态
     }
   },
   computed: {},
@@ -67,6 +75,50 @@ export default {
     async loadArtcitle () {
       const { data } = await getArticleById(this.articleId)
       this.article = data.data
+      // 数据改变影响视图更新（DOM数据）不是立即的
+      // 所以如果需要在修改数据之后马上操作被该数据影响的视图 DOM，需要把这个代码放到 $nextTick 中
+
+      // this.$nextTick 是 Vue 提供的一个方法
+      // 参考文档：
+      this.$nextTick(() => {
+        this.handlePerviewImage()
+      })
+    },
+    handlePerviewImage () {
+      // 1. 获取文章内容 DOM 容器
+      const articleContent = this.$refs['article-content']
+
+      // 2. 得到所有的 img 标签
+      const imgs = articleContent.querySelectorAll('img')
+
+      const imgPaths = [] // 收集所有的图片路径
+
+      // 3. 循环 img 列表，给 img 注册点击事件
+      imgs.forEach((img, index) => {
+        imgPaths.push(img.src)
+        img.onclick = function () {
+          // 4. 在事件处理函数中调用 ImagePreview() 预览
+          ImagePreview({
+            images: imgPaths, // 预览图片路径列表
+            startPosition: index // 起始位置
+          })
+        }
+      })
+    },
+    async onFollow () {
+      this.isFollowLoading = true
+      if (this.article.is_followed) {
+        // 已关注，取消关注
+        await deleteFollow(this.article.aut_id)
+        // this.article.is_followed = false
+      } else {
+        // 没有关注，添加关注
+        await addFollow(this.article.aut_id)
+        // this.article.is_followed = true
+      }
+      // 更新关注状态
+      this.article.is_followed = !this.article.is_followed
+      this.isFollowLoading = false
     }
   }
 }
